5332bd76763f1b01795052a3c69e55965eccd3db
mcflinta <93594910+mcflinta@users.noreply.github.com>
2024-05-23 13:20:32 +0700

    Add files via upload

--- hello.py
+++ hello.py
@@ -21,31 +21,11 @@ def main(args):
     model = YOLO('yolov8n.pt')  # load an official model
     # Predict with the model and save the results
     results = model('https://ultralytics.com/images/bus.jpg', save=True)
-    
+    save_dir =""    
     # Get the directory where the results are saved
     for result in results:
         save_dir = result.save_dir
-        break  # since there's only one result, we can break after finding it
 
-    # Initialize the path to the jpg image
-    image_path = None
-    
-    # Traverse the save_dir and find the single JPG image
-    for root, dirs, files in os.walk(save_dir):
-        for file in files:
-            if file.lower().endswith('.jpg'):
-                image_path = os.path.join(root, file)
-                break  # since there's only one image, we can break after finding it
-        if image_path:
-            break
-    
-    # Read and encode the image to base64
-    if image_path:
-        with open(image_path, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-            img_tag = f'<img src="data:image/jpeg;base64,{encoded_string}"/>'
-    else:
-        img_tag = '<p>No image found</p>'
     
     #return {"body": f"<html><body>{img_tag}</body></html>"}
-    return {"body": str(encoded_string)}
+    return {"body": str(save_dir)}
